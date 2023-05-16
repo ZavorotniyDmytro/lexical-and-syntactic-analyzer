e515d90799a888ee303c572d05f8b9a2bdc14821
Zavorotniy Dmytro <121565594+ZavorotniyDmytro@users.noreply.github.com>
2023-05-16 22:38:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from syntax import *
 HTML_FILE_PATH = 'index.html'
 
 if __name__ == '__main__':
-    html_string = file_to_html(HTML_FILE_PATH)
-    expression = lexis_analyzer(html_string)
+    # html_string = file_to_html(HTML_FILE_PATH)
+    # expression = lexis_analyzer(html_string)
+    # print(expression)
+    # predictive_syntax_analyzer(expression)
+
+    expression = lexis_analyzer('<h2 align="right">Заворотній Дмитро Олександрович, 202 група</h2>')
     print(expression)
     predictive_syntax_analyzer(expression)
+
+
